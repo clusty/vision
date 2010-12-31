7779d804623111181cfb755aa1627cde88bbbe7a
Vlad Lazar <clusty1@gmail.com>
2010-12-31 02:15:00 -0500

    convolution unseparated works

--- clTrack/cl2DConvolutionKernelSeparable.cpp
+++ clTrack/cl2DConvolutionKernelSeparable.cpp
@@ -21,26 +21,33 @@ std::string cl2DConvolutionKernelSeparable::getSource()
 {
 	std::ifstream inFile("/Users/clusty/Documents/code/git/vision/clTrack/ConvolutionSeparable.cl");
 	std::string code((std::istreambuf_iterator<char>(inFile)), std::istreambuf_iterator<char>());
+	inFile.close();
 	return code;
 }
 
 int cl2DConvolutionKernelSeparable::run()
 {
 	const unsigned int imageW = 1920;
-    const unsigned int imageH = 1080;
+    const unsigned int imageH = 1072;
 	//host buffers
 	cl_float *h_Input, *h_Output;
-	cl_float h_kernel[] = {0.006, 0.061, 0.242, 0.383, 0.242, 0.061, 0.006};
-	const unsigned int kSize = 7;
+	const unsigned int kSize = 3;
+	cl_float h_kernel[] = {    0.4038,    0.8021,    0.4038,
+		0.8021,   -4.8233,    0.8021,
+		0.4038,    0.8021,    0.4038};
+	
 	h_Input = new cl_float[imageW * imageH];
 	h_Output = new cl_float[imageW * imageH];
-	
-	srand(2009);
-#pragma omp parallel for
-	for (int i=0;i<imageH * imageW; ++i)
-	{
-		h_Input[i] = (cl_float)(rand() % 16);
+
+	std::ifstream imag("/Users/clusty/Documents/code/git/vision/clTrack/B_ANTOINETTE_3DTRACK_LONG.0001.pgm");
+	std::string dummy;
+	std::getline(imag, dummy);std::getline(imag, dummy);std::getline(imag, dummy);std::getline(imag, dummy);
+	std::cout<<dummy;
+		for (int i=0;i<imageH * imageW; ++i)
+		{
+		imag>>h_Input[i];
 	}
+	imag.close();
 	
 	if (!_initialized)
 	{
@@ -57,7 +64,7 @@ int cl2DConvolutionKernelSeparable::run()
 	
 	try{
 		clConvoKernel = cl::Buffer(env->getContext(), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 
-								   kSize * sizeof(cl_float), h_kernel, &err);
+								   kSize*kSize * sizeof(cl_float), h_kernel, &err);
 		clInput = cl::Buffer(env->getContext(), CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, 
 							 imageH * imageW * sizeof(cl_float), h_Input, &err);
 		clOutput = cl::Buffer(env->getContext(), CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, 
@@ -70,14 +77,13 @@ int cl2DConvolutionKernelSeparable::run()
 		_clKernel.setArg(3, imageW);
 		_clKernel.setArg(4, kSize);
 		
-		queue.enqueueNDRangeKernel( _clKernel, 
-								   cl::NullRange, 
-								   cl::NDRange(4,4),
-								   cl::NullRange,
-								   NULL,
-								   &event); 
+
+		queue.enqueueNDRangeKernel(_clKernel, cl::NullRange, 
+								   cl::NDRange(imageW, imageH), 
+								   cl::NDRange(32,16), NULL, &event);
 		event.wait();
-		std::cout<<"ran"<<std::endl;
+		queue.enqueueReadBuffer(clOutput,CL_FALSE, 0, imageH * imageW * sizeof(cl_float), h_Output, NULL, &event);
+		event.wait();
 	}
 	catch (cl::Error err) {
 		std::cerr  << "ERROR: "
@@ -88,6 +94,24 @@ int cl2DConvolutionKernelSeparable::run()
 		<< std::endl;
 		return -1;
 	} 
+	
+	std::ofstream orig("/Users/clusty/Documents/code/git/vision/clTrack/orig.pgm");
+	std::ofstream convo("/Users/clusty/Documents/code/git/vision/clTrack/convo.pgm");
+	
+	orig<<"P2\n#blahblah\n"<<imageW<<" "<<imageH<<std::endl<<"255\n";
+	convo<<"P2\n#blahblah\n"<<imageW<<" "<<imageH<<std::endl<<"255\n";
+	for (int i=0;i<imageH * imageW; ++i) {
+		orig<<static_cast<int>(h_Input[i])<<" ";
+		convo<<static_cast<int>(h_Output[i])<<" ";
+	}
+	
+	orig.close();
+	convo.close();
+	queue.enqueueReadBuffer(clConvoKernel,CL_FALSE, 0, kSize * sizeof(cl_float), h_Output, NULL, &event);
+	event.wait();
+
+	for (int i=0;i<kSize*kSize;i++)
+		std::cout<<h_Output[i]<<" ";
 	
 	delete[] h_Input;
 	delete[] h_Output;
--- clTrack/main.cpp
+++ clTrack/main.cpp
@@ -2,8 +2,6 @@
 #include "cl2DConvolutionKernelSeparable.h"
 int main (int argc, char * const argv[]) {
 	cl2DConvolutionKernelSeparable dummy("Convolve");
-	// std::cout << "Hello, World!\n";
-
 	return dummy.run();
 	
 }
