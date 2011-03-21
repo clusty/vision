6a2b3c8012ec9d48c3db261dafdd4351e3e47312
Vlad Lazar <clusty1@gmail.com>
2011-03-21 08:47:22 -0400

    Merge remote branch 'origin/master'
    Conflicts:
    	ScaledCG/Makefile
    	ScaledCG/banana.h
    	ScaledCG/main.C

--- ScaledCG/banana.h
+++ ScaledCG/banana.h
@@ -1,11 +1,26 @@
-#include "optimFunction.h"
+#include "optimFunction.hpp"
+template<typename T>
+class rosenbrock:public optimFunction<T>{
+   private:
+      template<typename G>
+         inline G evalFDummy(std::vector<G> &x)
+         {
+            G sum=0;
+	         for (int i=0;i<x.size()-1;i++)
+               sum=sum+pow(1-x[i],2)+100*pow(x[i+1]-x[i]*x[i],2);
+	         return sum;
 
-class rosenbrock:public optimFunction{
-   public:   virtual double evalF(std::vector<double>&x)
+         }
+   public:   virtual T evalF(std::vector<T>&x)
              {
-                double sum=0;
-	             for (int i=0;i<x.size()-1;i++)
-                   sum+=pow(1-x[i],2)+100*pow(x[i+1]-x[i]*x[i],2);
-	             return sum;
+                return evalFDummy(x);
              }
+
+             virtual CppAD::AD<T> evalF(std::vector<CppAD::AD< T> >&x)
+             {
+                return evalFDummy(x);
+             }
+
+             rosenbrock(int ndim):
+                optimFunction<T>(ndim){}
 };
